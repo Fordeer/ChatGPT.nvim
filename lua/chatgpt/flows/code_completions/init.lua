cedc179a2114c62b160007446411827afa9602cc
Alex <dinhhungvn@gmail.com>
2023-03-25 23:15:40 +0700

    Change new mode complete

--- lua/chatgpt/flows/code_completions/init.lua
+++ lua/chatgpt/flows/code_completions/init.lua
@@ -52,7 +52,7 @@ M.complete = function()
   set_loading(true)
 
   Api.completions({
-    model = "code-davinci-002",
+    model = "text-davinci-003",
     prompt = prefix,
     suffix = suffix,
     max_tokens = 2048,
